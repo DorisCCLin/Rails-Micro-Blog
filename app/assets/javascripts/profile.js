ee8d0d672382919b7b5ae63ed09601fa70b0218e
DorisCCLin <doriscclin@gmail.com>
2018-03-03 23:52:53 -0500

    profile styling done

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,29 @@
+console.log('hello')
+var update_user = document.getElementById('update_user');
+var hidden_user_update = document.getElementById('hidden_user_update');
+var cancel_botton = document.getElementById('cancel_botton');
+
+
+if(update_user) {
+update_user.addEventListener('click', function(){
+	hidden_user_update.style.visibility = 'visible';
+});
+}
+
+cancel_botton.addEventListener('click', function(){
+	hidden_user_update.style.visibility = 'hidden';
+})
+
+
+var coll = document.getElementById("new_post");
+
+
+  coll.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
